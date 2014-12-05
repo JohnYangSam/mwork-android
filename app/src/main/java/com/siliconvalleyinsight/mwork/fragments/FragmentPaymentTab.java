66d79b4840714378b9fc86ff1c346536fb8a71ac
John YS <johnys@stanford.edu>
2014-12-05 05:25:22 -0800

    link in cashout screen

--- app/src/main/java/com/siliconvalleyinsight/mwork/fragments/FragmentPaymentTab.java
+++ app/src/main/java/com/siliconvalleyinsight/mwork/fragments/FragmentPaymentTab.java
@@ -1,12 +1,15 @@
 package com.siliconvalleyinsight.mwork.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.siliconvalleyinsight.mwork.R;
+import com.siliconvalleyinsight.mwork.activities.BetterCashOutActivity;
 
 public class FragmentPaymentTab extends Fragment {
 
@@ -18,7 +21,18 @@ public class FragmentPaymentTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_payment_tab, container, false);
+        final View v = inflater.inflate(R.layout.fragment_payment_tab, container, false);
+
+        Button mCashOutButton = (Button) v.findViewById(R.id.cash_out_btn);
+
+        mCashOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(v.getContext(), BetterCashOutActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
 }
