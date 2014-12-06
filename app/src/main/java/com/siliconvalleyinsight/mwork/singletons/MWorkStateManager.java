0ddc329d0b9514a82ff754814a71d77761405803
John YS <johnys@stanford.edu>
2014-12-06 09:54:16 -0800

    add portrait freeze

--- app/src/main/java/com/siliconvalleyinsight/mwork/singletons/MWorkStateManager.java
+++ app/src/main/java/com/siliconvalleyinsight/mwork/singletons/MWorkStateManager.java
@@ -5,18 +5,36 @@ package com.siliconvalleyinsight.mwork.singletons;
  */
 public class MWorkStateManager {
     private boolean mLogin;
+    private double mBalance;
+    private int mTasksCompleted;
+
+    private Profile mProfile;
+
+    private class Profile {
+        public String language;
+        public int age;
+        public String City;
+        public String State;
+        public String Country;
+    }
 
     // Fast initialization
     MWorkStateManager() {
         mLogin = false;
+        mBalance = 0;
+        mTasksCompleted = 0;
+
     }
 
     // Longer Initialization here (lazy init) if we needed it
     public boolean getLogin() {
         return mLogin;
     }
-
     public void setLogin(boolean state) {
         mLogin = state;
+    }
+
+    public double getBalance() {
+        return mBalance;
     }
 }
